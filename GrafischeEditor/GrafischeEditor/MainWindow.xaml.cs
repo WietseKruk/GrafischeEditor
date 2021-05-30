3e2907874c6dc3ed81265da5c191a6f19fc16356
WietseKruk <wietsekruk@gmail.com>
2021-05-30 15:59:47 +0200

    draggable shapes

--- GrafischeEditor/GrafischeEditor/MainWindow.xaml.cs
+++ GrafischeEditor/GrafischeEditor/MainWindow.xaml.cs
@@ -31,7 +31,7 @@ namespace GrafischeEditor
 
 
 
-        string mouseMode = "select"; // Keeps track of which Tool is selected, default is select
+        bool selectMode = false; // Keeps track of which Tool is selected, default is select
 
         bool mouseDown = false; // Set to true when mouse is held down
         Point mouseDownPos; // The position of the mouse when clicked down
@@ -51,24 +51,28 @@ namespace GrafischeEditor
         private selectedShape Shape1 = selectedShape.None;
 
         private void Grid_MouseDown(object sender, MouseButtonEventArgs e){
-            // Capture and track the mouse
-            mouseDown = true;
-            mouseDownPos = e.GetPosition(theGrid);
-            theGrid.CaptureMouse();
-            selectedSelectionArea = null;
 
-            switch (Shape1) {
-                case selectedShape.Rectangle:
-                    selectedSelectionArea = selectionBox;
-                    break;
-                case selectedShape.Ellipse:
-                    selectedSelectionArea = selectionEllipse;
-                    break;
-                default:
-                    return;
+            if (!selectMode)
+            {
+                // Capture and track the mouse
+                mouseDown = true;
+                mouseDownPos = e.GetPosition(theGrid);
+                theGrid.CaptureMouse();
+                selectedSelectionArea = null;
+
+                switch (Shape1)
+                {
+                    case selectedShape.Rectangle:
+                        selectedSelectionArea = selectionBox;
+                        break;
+                    case selectedShape.Ellipse:
+                        selectedSelectionArea = selectionEllipse;
+                        break;
+                    default:
+                        return;
+                }
+                drawSelectionArea(selectedSelectionArea, (mouseDownPos.X, mouseDownPos.Y));
             }
-            drawSelectionArea(selectedSelectionArea, (mouseDownPos.X, mouseDownPos.Y));
-
         }
 
 
@@ -86,45 +90,49 @@ namespace GrafischeEditor
         }
 
         private void Grid_MouseUp(object sender, MouseButtonEventArgs e) {
-            // Release mouse capture and stop tracking
-            mouseDown = false;
-            theGrid.ReleaseMouseCapture();
 
-            // Hide selection box again
-            selectedSelectionArea.Visibility = Visibility.Collapsed;
-            selectedSelectionArea = null;
-
-            Point mouseUpPos = e.GetPosition(theGrid);
-
-            // TODO Check what is selected
-
-            double height;
-            double width;
-
-            double endPointX = mouseDownPos.X;
-            double endPointY = mouseDownPos.Y;
-
-            if (mouseDownPos.X < mouseUpPos.X)
+            if (!selectMode)
             {
-                width = mouseUpPos.X - mouseDownPos.X;
-            }
-            else
-            {
-                width = mouseDownPos.X - mouseUpPos.X;
-                endPointX = mouseUpPos.X;
-            }
+                // Release mouse capture and stop tracking
+                mouseDown = false;
+                theGrid.ReleaseMouseCapture();
 
-            if (mouseDownPos.Y < mouseUpPos.Y)
-            {
-                height = mouseUpPos.Y - mouseDownPos.Y;
-            }
-            else
-            {
-                height = mouseDownPos.Y - mouseUpPos.Y;
-                endPointY = mouseUpPos.Y;
-            }
+                // Hide selection box again
+                selectedSelectionArea.Visibility = Visibility.Collapsed;
+                selectedSelectionArea = null;
 
-            drawShape(Shape1, getSelectedColorBrush(selectedColor), height, width, endPointX, endPointY);
+                Point mouseUpPos = e.GetPosition(theGrid);
+
+                // TODO Check what is selected
+
+                double height;
+                double width;
+
+                double endPointX = mouseDownPos.X;
+                double endPointY = mouseDownPos.Y;
+
+                if (mouseDownPos.X < mouseUpPos.X)
+                {
+                    width = mouseUpPos.X - mouseDownPos.X;
+                }
+                else
+                {
+                    width = mouseDownPos.X - mouseUpPos.X;
+                    endPointX = mouseUpPos.X;
+                }
+
+                if (mouseDownPos.Y < mouseUpPos.Y)
+                {
+                    height = mouseUpPos.Y - mouseDownPos.Y;
+                }
+                else
+                {
+                    height = mouseDownPos.Y - mouseUpPos.Y;
+                    endPointY = mouseUpPos.Y;
+                }
+
+                drawShape(Shape1, getSelectedColorBrush(selectedColor), height, width, endPointX, endPointY);
+            }
         }
 
 
@@ -142,7 +150,8 @@ namespace GrafischeEditor
                         Width = width,
                         Visibility = Visibility.Visible,
                         Fill = getSelectedColorBrush(selectedColor),
-                        Stroke = getSelectedColorBrush(selectedColor)
+                        Stroke = getSelectedColorBrush(selectedColor),
+ 
                     };
                     
                     break;
@@ -163,21 +172,80 @@ namespace GrafischeEditor
                     return;            
             }
 
-            Console.WriteLine("Drawing " + Shape1 + " with size " + height + "x" + width + " and color " + selectedColor);
+        Console.WriteLine("Drawing " + Shape1 + " with size " + height + "x" + width + " and color " + selectedColor);
 
 
             if (renderShape != null)
             {
+                string name = selectedColor + Shape1.ToString();
+                // Update size and color
+                renderShape.Name = name;
+                renderShape.Height = height;
+                renderShape.Width = width;
+                renderShape.Visibility = Visibility.Visible;
+                renderShape.Fill = getSelectedColorBrush(selectedColor);
+                renderShape.Stroke = getSelectedColorBrush(selectedColor);
+
+                // Add mousebutton event handlers for dragging/resizing
+                renderShape.MouseDown += new MouseButtonEventHandler(shape_MouseDown);
+                renderShape.MouseUp += new MouseButtonEventHandler(shape_MouseUp);
+                renderShape.MouseMove += new MouseEventHandler(shape_MouseMove);
+
+                // Set position on canvas
                 Canvas.SetLeft(renderShape, endPointX);
                 Canvas.SetTop(renderShape, endPointY);
 
+                //add to canvas
                 canvas.Children.Add(renderShape);
+
+                // Reset renderShape
                 renderShape = null;
-                colorBrush = null;
             }
             else { Console.WriteLine("No shape selected"); }
+
         }
 
+
+        protected bool isDragging;
+        private Point clickPosition;
+        private TranslateTransform originTT;
+
+
+        private void shape_MouseMove(object sender, MouseEventArgs e)
+        {
+            Shape selectedshape = sender as Shape;
+            if (selectMode && isDragging && selectedshape != null) {
+                Point currentPos = e.GetPosition(canvas);
+                var transform = selectedshape.RenderTransform as TranslateTransform ?? new TranslateTransform();
+                transform.X = originTT.X + (currentPos.X - clickPosition.X);
+                transform.Y = originTT.Y + (currentPos.Y - clickPosition.Y);
+                selectedshape.RenderTransform = new TranslateTransform(transform.X, transform.Y);  
+                Console.WriteLine("Dragging...");
+            }
+        }
+
+        private void shape_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            if (selectMode && isDragging)
+                isDragging = false;
+                Shape selectedshape = sender as Shape;
+                selectedshape.ReleaseMouseCapture();
+        
+                Console.WriteLine("Dropping...");
+        }
+
+        private void shape_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (selectMode) {
+                Shape selectedshape = sender as Shape;
+                Console.WriteLine(selectedshape.Name);
+                
+                originTT = selectedshape.RenderTransform as TranslateTransform ?? new TranslateTransform();
+                isDragging = true;
+                clickPosition = e.GetPosition(canvas);
+                selectedshape.CaptureMouse();
+            }
+        }
 
         private void Grid_MouseMove(object sender, MouseEventArgs e){
             if (mouseDown && selectedSelectionArea != null)
@@ -215,17 +283,27 @@ namespace GrafischeEditor
         private void selectEllipse_Click(object sender, RoutedEventArgs e)
         {
             Shape1 = selectedShape.Ellipse;
+
+            updateToolsAndShapes();
+
             selectEllipse.Background = new SolidColorBrush(Color.FromRgb(119, 136, 153));
-            selectRectangle.Background = new SolidColorBrush(Color.FromRgb(211, 211, 211));
         }
 
         private void selectRectangle_Click(object sender, RoutedEventArgs e)
         {
             Shape1 = selectedShape.Rectangle;
+
+            updateToolsAndShapes();
+
             selectRectangle.Background = new SolidColorBrush(Color.FromRgb(119, 136, 153));
-            selectEllipse.Background = new SolidColorBrush(Color.FromRgb(211, 211, 211));
         }
 
+        private void updateToolsAndShapes() {
+            selectEllipse.Background = new SolidColorBrush(Color.FromRgb(211, 211, 211));
+            selectRectangle.Background = new SolidColorBrush(Color.FromRgb(211, 211, 211));
+            selectSelector.Background = new SolidColorBrush(Color.FromRgb(211, 211, 211));
+            selectMode = false;
+        }
 
         private SolidColorBrush getSelectedColorBrush(string selectedColor)
         {
@@ -286,12 +364,21 @@ namespace GrafischeEditor
 
         private void selectSelector_Click(object sender, RoutedEventArgs e)
         {
-            mouseMode = "select";
+            
+            updateToolsAndShapes();
+            selectSelector.Background = new SolidColorBrush(Color.FromRgb(119, 136, 153));
+            Shape1 = selectedShape.None;
+            selectMode = true;
         }
 
         private void selectClear_Click(object sender, RoutedEventArgs e)
         {
             canvas.Children.RemoveRange(2, (canvas.Children.Count - 2));
+        }
+
+        private void selectResizer_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
